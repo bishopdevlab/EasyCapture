cc4f8a03e5f8061aaae5b17fdfffcf4279d5e055
Yong-il Lee <dbjavapia@gmail.com>
2021-03-18 00:48:38 +0900

    First Commit
    1st release

---
+++ EasyCapture/EasyCapture/Form.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace EasyCapture
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.Capture = new System.Windows.Forms.Button();
+            this.numericFileName = new System.Windows.Forms.NumericUpDown();
+            this.ScreenAll = new System.Windows.Forms.RadioButton();
+            this.ScreenPrimary = new System.Windows.Forms.RadioButton();
+            this.ScreenSecondary = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.numericFileName)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Capture
+            // 
+            this.Capture.Location = new System.Drawing.Point(242, 9);
+            this.Capture.Name = "Capture";
+            this.Capture.Size = new System.Drawing.Size(70, 23);
+            this.Capture.TabIndex = 0;
+            this.Capture.Text = "Capture";
+            this.Capture.UseVisualStyleBackColor = true;
+            this.Capture.Click += new System.EventHandler(this.Capture_Click);
+            // 
+            // numericFileName
+            // 
+            this.numericFileName.Location = new System.Drawing.Point(146, 9);
+            this.numericFileName.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.numericFileName.Name = "numericFileName";
+            this.numericFileName.Size = new System.Drawing.Size(90, 21);
+            this.numericFileName.TabIndex = 1;
+            // 
+            // ScreenAll
+            // 
+            this.ScreenAll.AutoSize = true;
+            this.ScreenAll.Enabled = false;
+            this.ScreenAll.Location = new System.Drawing.Point(11, 12);
+            this.ScreenAll.Name = "ScreenAll";
+            this.ScreenAll.Size = new System.Drawing.Size(37, 16);
+            this.ScreenAll.TabIndex = 2;
+            this.ScreenAll.Text = "All";
+            this.ScreenAll.UseVisualStyleBackColor = true;
+            this.ScreenAll.CheckedChanged += new System.EventHandler(this.Screen_CheckedChanged);
+            // 
+            // ScreenPrimary
+            // 
+            this.ScreenPrimary.AutoSize = true;
+            this.ScreenPrimary.Checked = true;
+            this.ScreenPrimary.Location = new System.Drawing.Point(54, 12);
+            this.ScreenPrimary.Name = "ScreenPrimary";
+            this.ScreenPrimary.Size = new System.Drawing.Size(31, 16);
+            this.ScreenPrimary.TabIndex = 3;
+            this.ScreenPrimary.TabStop = true;
+            this.ScreenPrimary.Text = "P";
+            this.ScreenPrimary.UseVisualStyleBackColor = true;
+            this.ScreenPrimary.CheckedChanged += new System.EventHandler(this.Screen_CheckedChanged);
+            // 
+            // ScreenSecondary
+            // 
+            this.ScreenSecondary.AutoSize = true;
+            this.ScreenSecondary.Location = new System.Drawing.Point(91, 12);
+            this.ScreenSecondary.Name = "ScreenSecondary";
+            this.ScreenSecondary.Size = new System.Drawing.Size(31, 16);
+            this.ScreenSecondary.TabIndex = 4;
+            this.ScreenSecondary.Text = "S";
+            this.ScreenSecondary.UseVisualStyleBackColor = true;
+            this.ScreenSecondary.CheckedChanged += new System.EventHandler(this.Screen_CheckedChanged);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 41);
+            this.Controls.Add(this.ScreenSecondary);
+            this.Controls.Add(this.ScreenPrimary);
+            this.Controls.Add(this.ScreenAll);
+            this.Controls.Add(this.numericFileName);
+            this.Controls.Add(this.Capture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form";
+            this.Text = "EasyCapture - Copyright by Bishop";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericFileName)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Capture;
+        private System.Windows.Forms.NumericUpDown numericFileName;
+        private System.Windows.Forms.RadioButton ScreenAll;
+        private System.Windows.Forms.RadioButton ScreenPrimary;
+        private System.Windows.Forms.RadioButton ScreenSecondary;
+    }
+}
+
---
+++ EasyCapture/EasyCapture/Form.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyCapture
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private Timer myTopmostChecker;
+        private Rectangle myCaptureArea;
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            myTopmostChecker = new Timer();
+            myTopmostChecker.Tick += MyTopmostChecker_Tick;
+            myTopmostChecker.Interval = 500;
+            myTopmostChecker.Start();
+
+            SelectScreen(ScreenPrimary.Name);
+        }
+
+        private void MyTopmostChecker_Tick(object sender, EventArgs e)
+        {
+            ChangeTopMost();
+        }
+
+        private void ChangeTopMost()
+        {
+            this.TopMost = true;
+            this.Focus();
+            this.BringToFront();
+            this.TopMost = false;
+        }
+
+        private void Capture_Click(object sender, EventArgs e)
+        {
+            myTopmostChecker.Stop();
+            this.Hide();
+            CaptureScreen();
+            this.Show();
+            myTopmostChecker.Start();
+        }
+
+        private void CaptureScreen()
+        {
+            try
+            {
+                var directory = @"d:\Capture";
+                var fileName = string.Format("{0}.png", numericFileName.Value);
+                var filePath = Path.Combine(directory, fileName);
+
+                if (!Directory.Exists(directory))
+                    Directory.CreateDirectory(directory);
+
+                Rectangle s_rect = myCaptureArea;
+                using (Bitmap bmp = new Bitmap(s_rect.Width, s_rect.Height))
+                {
+                    using (Graphics gScreen = Graphics.FromImage(bmp))
+                        gScreen.CopyFromScreen(s_rect.Location, Point.Empty, s_rect.Size);
+                    bmp.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Screen_CheckedChanged(object sender, EventArgs e)
+        {
+            var selectedRadioButton = (sender as RadioButton);
+            
+            if (selectedRadioButton.Checked == true)
+                SelectScreen(selectedRadioButton.Name);
+        }
+
+        private void SelectScreen(string targetScreen)
+        {
+            switch (targetScreen)
+            {
+                case "ScreenAll":
+                    if (Screen.AllScreens.Length == 1)
+                        myCaptureArea = Screen.PrimaryScreen.Bounds;
+                    if (Screen.AllScreens.Length > 1)
+                    {
+                        // L/R
+                        var left = Screen.AllScreens[0].Bounds.Left;
+                        var top = Screen.AllScreens[0].Bounds.Top;
+                        var righ = Screen.AllScreens[1].Bounds.Right;
+                        var bottom = Screen.AllScreens[1].Bounds.Bottom;
+                        myCaptureArea = new Rectangle(left, top, righ - left, bottom - top);
+
+                        // U/D
+                        //var left = Screen.AllScreens[0].Bounds.Left;
+                        //var top = Screen.AllScreens[0].Bounds.Top;
+                        //var righ = Screen.AllScreens[0].Bounds.Right;
+                        //var bottom = Screen.AllScreens[1].Bounds.Bottom;
+                        //myCaptureArea = new Rectangle(left, top, righ - left, bottom - top);
+                    }
+                    break;
+                case "ScreenPrimary":
+                    myCaptureArea = Screen.PrimaryScreen.Bounds;
+                    break;
+                case "ScreenSecondary":
+                    if (Screen.AllScreens.Length == 1)
+                    {
+                        ScreenPrimary.Checked = true;
+                        break;
+                    }
+                    myCaptureArea = Screen.AllScreens[1].Bounds;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
